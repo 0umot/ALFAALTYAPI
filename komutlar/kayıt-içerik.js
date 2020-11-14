e3fca7705056cd02a7f4847e9e5e2e5eb4fb5b0a
Glitch (clooudup) <none>
2020-11-14 17:03:22 +0000

    🚛🍟 Checkpoint ./komutlar/ban-sistem.js:23311033/11 ./komutlar/diğer.js:23311033/132 ./komutlar/fake-hesap-sistem.js:23311033/37 ./komutlar/jail-sistem.js:23311033/13 ./komutlar/kayıt-içerik.js:23311033/140 ./komutlar/kayıt-mesaj.js:23311033/15 ./komutlar/komut-mesaj-sistem.js:23311033/15 ./komutlar/kısıtlamalar.js:23311033/11 ./komutlar/mesaj-tag.js:23311033/13 ./komutlar/mute-sistem.js:23311033/13 ./komutlar/mute.js:23311033/24 ./komutlar/oto-isim-sistem.js:23311033/23

--- komutlar/kayıt-içerik.js
+++ komutlar/kayıt-içerik.js
@@ -7,8 +7,8 @@ exports.run = async (client, message, args) => {
   }
 
 let images = ['https://media.giphy.com/media/mBkM18U5OMSkTcDmeu/giphy.gif', 'https://media.giphy.com/media/RGRzukK0YNlQbZEUVP/giphy.gif'];
-message.channel.send(new Discord.MessageEmbed().setColor('#000001').setDescription(`**CloudUP Ana Menüsüne Hoş Geldin Dostum :innocent:
-${client.user} Kullanırken \`@CloudUP\` rolünü en yukarıda tutunuz.**
+message.channel.send(new Discord.MessageEmbed().setColor('#000001').setDescription(`**Alfa Ana Menüsüne Hoş Geldin Dostum :innocent:
+${client.user} Kullanırken \`@Alfa\` rolünü en yukarıda tutunuz.**
 **
 🐝 \`${client.ayarlar.prefix}diğer\`
 Gerekli olabilecek komutlar
@@ -46,8 +46,8 @@ Aktif Koruma Sistem **7 Adet Sunucunuz için koruma sistem aktif edersin**
 **💸 İninal Barkod NO: \`123456789123\`
 💸 Papara Hesap NO: \`1234567891\`
 
-💐 CloudUP Youtube Eğitim: :)
-**`).setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.com/8e70d198-9ddc-40aa-b0c6-ccb4573f14a4%2F6499d2f1c46b106eed1e25892568aa55.png').setImage(images.random()))
+💐 Alfa Youtube Eğitim: :)
+**`).setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https').setImage(images.random()))
 
 };
 exports.conf = {
