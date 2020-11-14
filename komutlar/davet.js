c3d19a056e0a9010048e08046abc91426dba0fc6
Glitch (clooudup) <none>
2020-11-14 17:49:31 +0000

    💨👁 Checkpoint ./komutlar/davet.js:23311033/1410

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -6,14 +6,12 @@ message.channel.send(new Discord.MessageEmbed()
 .setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.com/8e70d198-9ddc-40aa-b0c6-ccb4573f14a4%2F6499d2f1c46b106eed1e25892568aa55.png')
 .setDescription(`>>> ${client.user} kullanırken lütfen \`@${client.user.username}\` rolünü en yukarıda tutunuz!
 
-**Rollü Davet İçin:** [Buraya tıkla](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)
-\`\`\`discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8\`\`\`
+**Rollü Davet İçin:** [Buraya tıkla](https://discord.com/oauth2/authorize?client_id=777220232736931880&scope=bot&permissions=8)
 
-**Rolsüz Davet İçin:** [Buraya tıkla](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=0)
-\`\`\`discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=0\`\`\`
+**Rolsüz Davet İçin:** [Buraya tıkla](https://discord.com/oauth2/authorize?client_id=777220232736931880&scope=bot&permissions=0)
 
-**Rollü Davet İçin:** [Buraya tıkla](https://discord.com/invite/codare)
-\`\`\`discord.gg/codare\`\`\``));
+**Rollü Davet İçin:** [Buraya tıkla](https://discord.gg/kYSJneDpNf)
+\`\`\`https://discord.gg/kYSJneDpNf\`\`\``));
 
 };
 exports.conf = {
